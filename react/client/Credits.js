3c5d47366ff2b884f2a1cae04de3c550837f529a
Matthew <m_kmorgan@hotmail.com>
2021-01-04 16:17:50 -0700

    Finished Credits Scene

--- react/client/Credits.js
+++ react/client/Credits.js
@@ -13,19 +13,43 @@ class  Credits extends Master {
     /*  PLAYER  */
     state.player = this.createGravityPlayer(250,550)//
     state.player.body.setGravityY(0);
-    // state.player = this.physics.add.sprite(250,550, 'player', 3);
-    // state.player.angle = -10;
 
     /*  TITLE  */
     state.gameTitle = this.createText(55, 110, 'CREDITS', '100px');
     state.gameTitle.angle = -10;
 
+    /*  CREDITS  */
+    state.nameX = 600;
+    state.titleX = 550;
+    state.matthewMorgan = this.add.group();
+    state.creditTitle = this.add.group();
+    state.creditTitle.add(this.createText(state.titleX, 100, 'GAME ENGINE:', '75px'));
+    state.phaserText = this.createText(state.nameX, 175, 'PHASER.JS ', '75px');
+    state.creditTitle.add(this.createText(state.titleX, 300, 'ART & ANIMATION:', '75px'));
+    state.matthewMorgan.add(this.createText(state.nameX, 375, 'Matthew Morgan', '75px'));
+    state.creditTitle.add(this.createText(state.titleX, 500, 'CONCEPT & DESIGN:', '75px'));
+    state.matthewMorgan.add(this.createText(state.nameX, 575, 'Matthew Morgan', '75px'));
+    state.creditTitle.add(this.createText(state.titleX, 700, 'PROGRAMING:', '75px'));
+    state.matthewMorgan.add(this.createText(state.nameX, 775, 'Matthew Morgan', '75px'));
+
+    /*  HOVER AND LINKS  */
+    Phaser.Actions.Call(state.matthewMorgan.getChildren(), (matt) => {
+      matt.setInteractive().on('pointerup', () => {
+        window.open('https://github.com/mkmorgan1', '_blank');
+      });
+      this.textHoverFeature(matt);
+    })
+    state.phaserText.setInteractive().on('pointerup', () => {
+      window.open('https://phaser.io/phaser3', '_blank');
+    })
+    this.textHoverFeature(state.phaserText);
+
+    /*  ANIMATIONS  */
     this.createAnimations();
 
     state.player.move = this.tweens.add({
       targets: state.player,
       scale:1,
-      // x: state.player.x,
       ease: 'Linear',
       duration: 800,
       repeat: -1,
@@ -36,26 +60,19 @@ class  Credits extends Master {
         }
       }
     });
-
-    state.credits = this.createText(550, 100, 'GAME ENGINE:\n   PHASER.js\n\nART & ANIMATIONS:\n   MATTHEW MORGAN\n\nCONCEPT & DESIGN:\n   MATTHEW MORGAN\n\nPROGRAMING:\n   MATTHEW MORGAN', '75px');
-
-    state.credits.setInteractive().on('pointerup', () => {
-      window.open('https://github.com/mkmorgan1', '_blank');
-    })
-
-
     state.player.anims.play('dance');
-
     state.portal.anims.play('portal-grow').once('animationcomplete', () => {
       state.portal.anims.play('portal-open-forever');
     })
 
+    /*  MAIN MENUES  */
     state.backText = this.createText(50, 900, '< Main Menue', '75px', 5).setInteractive();
     this.textHoverFeature(state.backText);
     state.backText.on('pointerdown', () => {
       this.scene.start('StartScreen');
     }, this);
   }
+
   update() {
     state.player.angle += 1;
   }
