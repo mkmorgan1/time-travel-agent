ecf7759c17bc6b264812a9cea9aa0405ea8eba1d
Matthew <m_kmorgan@hotmail.com>
2022-06-14 01:27:02 -0600

    FEAT: created arrow keys class

---
+++ react/client/ArrowKeys.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+
+export default ArrowKeys = () => {
+  return (
+    <>
+      <div className='arrow-keys'>
+        <button>{'^'}</button>
+        <button>{'<'}</button>
+        <button>{'>'}</button>
+      </div>
+    </>
+  )
+}
